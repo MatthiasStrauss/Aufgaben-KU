ccddaf9305ed37480f76b37d2125627da070cd55
MatthiasStrauss <m.straus@tu-bs.de>
2018-04-28 17:40:01 +0200

    aufgabe4.2
    not working

---
+++ aufgabe4-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+/*
+Funktion y=x_1^3+7*x_2
+}*/
+bool teilbar(bool teilbar);
+main()
+{
+    int zahl;
+    int teiler;
+    bool a=false;
+    cout<< "Gib eine positive Zahl ein "<<endl;
+    cin>>zahl;
+    while (zahl<=0)
+    {
+        cout<< "Die Zahl ist negativ gib eine positive Zahl ein"<<endl;
+        cin>>zahl;
+    }
+    for (int i=2, i<10, i++)
+    {
+        a = teilbar(zahl,teiler);
+        if (a)
+        {
+            cout<<"Deine Zahl ist teilbar durch"<<i<<endl;
+            i=10;
+        }
+
+    }
+}
+
+bool teilbar(int zahl, int teiler)
+{
+    if(zahl%teiler==0)
+    {return true;}
+    else {return false;}
